73f3c333b541cd7a3220ac444a3e6d76d7ec35ae
Evan Tang <etang110@gmail.com>
2017-06-10 04:04:05 -0500

    Date formatters always use en-us locale

--- Sources/Sword/Utils/Utils.swift
+++ Sources/Sword/Utils/Utils.swift
@@ -22,6 +22,7 @@ extension String {
   /// Computed property to get date from string
   var date: Date {
     let dateFormat = DateFormatter()
+    dateFormat.locale = Locale(identifier: "en-us")
     dateFormat.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSSSSZ"
 
     if let returnDate = dateFormat.date(from: self) {
@@ -36,6 +37,7 @@ extension String {
   /// Computed property to get date from string (specifically the Date header from requests)
   var httpDate: Date {
     let dateFormat = DateFormatter()
+    dateFormat.locale = Locale(identifier: "en-us")
     dateFormat.dateFormat = "E, dd MMM yyyy HH:mm:ss zzzz"
 
     return dateFormat.date(from: self)!
